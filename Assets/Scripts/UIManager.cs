086ca169a133fb202490f07e103c9fa3faf0e262
JeonYJ05 <136886537+JeonYJ05@users.noreply.github.com>
2024-06-11 03:46:03 +0900

    add pauseMenu

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace YJ.PocketGame
+{
+    public class UIManager : MonoBehaviour
+    {
+        private bool IsPause;
+        public GameObject PauseUI;
+
+        private void Start()
+        {
+            IsPause = false;
+        }
+        private void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.Escape))
+            {
+                Toggle();
+            }
+        }
+        public void Toggle()
+        {
+            PauseUI.SetActive(!PauseUI.activeSelf);
+
+            if(PauseUI.activeSelf)
+            {
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                Time.timeScale = 1f;
+            }
+        }
+    }
+}
